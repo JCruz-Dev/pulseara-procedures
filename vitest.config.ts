a5eb98b14aaf7081438f887dfdc814a2affce65c
Juan Carlos Cruz de los Santos <juancarloscruz278@gmail.com>
2024-05-19 04:31:10 -0400

    feat: adds alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-
+import path from "path";
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@db": path.resolve(__dirname, "./src/db"),
+    },
+  },
   plugins: [react()],
   test: {
     include: ["**/*.test.tsx"],
     globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
   },
 });

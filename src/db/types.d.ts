d30b749848b418c1c32425fa2ddf0d0f515250b0
Juan Carlos Cruz de los Santos <juancarloscruz278@gmail.com>
2024-05-19 04:32:10 -0400

    refactor: changes base styling files and removes types file from db

--- src/db/types.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IProcedures {
-  id?: string;
-  procediment: string;
-  procediment_code: string;
-  reclaimed_amount: number;
-  procediment_difference: number;
-  insurance_authorized_amount: number;
-}

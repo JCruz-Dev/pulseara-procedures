69b49ff2ec4e510909639361e40391cafc59273a
Juan Carlos Cruz de los Santos <juancarloscruz278@gmail.com>
2024-05-19 04:34:05 -0400

    feat: adds dumb container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="container">{children}</div>;
+  return (
+    <div className="px-4 py-8 xl:px-[88px] xl:py-[99px] max-w-7xl mx-auto">
+      {children}
+    </div>
+  );
 };
 export default Layout;
